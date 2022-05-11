35fca1e0cd6ff80871ef09dcf24be03331dad135
drtocino <dilan_chuquimia@hotmail.com>
2022-05-11 12:34:17 -0400

    Implementacion de tailwind css y prueba con ui de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    UsuariosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
